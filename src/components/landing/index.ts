cca5da40fae106519dd79fed688e37c486e847d2
Vishnu Sai <63618609+SquaredPiano@users.noreply.github.com>
2026-01-18 11:36:38 -0500

    Delete src directory

--- src/components/landing/index.ts
+++
@@ -1,7 +0,0 @@
-export { Navbar } from "./Navbar";
-export { HeroSection } from "./HeroSection";
-export { CapabilitiesBanner } from "./CapabilitiesBanner";
-export { WorkSection } from "./WorkSection";
-export { ProcessSection } from "./ProcessSection";
-export { CTASection } from "./CTASection";
-export { Footer } from "./Footer";
